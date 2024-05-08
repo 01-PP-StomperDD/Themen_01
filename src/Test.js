ee162020c4ab791586469bc14f4b853f9854d53d
Stomper90845 <168742587+Stomper90845@users.noreply.github.com>
2024-05-08 09:54:37 +0000

    trash still trash

--- src/Test.js
+++ src/Test.js
@@ -4,3 +4,23 @@ firstName = "Chris"; // Wertzuweisung | assignment
 console.log(firstName); // Ausgabe
 let familyName = "dissmiss"; // Deklaration + Wertzuweisung
 console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // --> Hi, ich bin Max Mütze!
+
+let test;
+console.log("Inhalt: " + test);
+console.log("Datentyp " + typeof test);
+console.log("_______________");
+test = "setz dich Hund";
+console.log("Inhalt: " + test);
+console.log("Datentyp " + typeof test);
+console.log("_______________");
+test = "dont care";
+console.log("Inhalt: " + test);
+console.log("Datentyp " + typeof test);
+console.log("_______________");
+test = true;
+console.log("Inhalt: " + test);
+console.log("Datentyp " + typeof test);
+console.log("_______________");
+test = "0";
+console.log("Inhalt: " + test);
+console.log("Verzeichnis: " + test);
