f9d3fb90a96daf73bed15214645c1551232cae01
Stomper90845 <168742587+Stomper90845@users.noreply.github.com>
2024-05-10 08:58:46 +0000

    fine

--- src/age_logic.js
+++ src/age_logic.js
@@ -1,6 +1,6 @@
 let ageChris, ageDarma;
 const birthYearChris = 1980;
-const birthYearDarma = 1890;
+const birthYearDarma = 1979;
 
 // Berechnung Alter
 let date = new Date();
@@ -12,3 +12,12 @@ ageDarma = year - birthYearDarma;
 console.log("ageChris: " + ageChris);
 console.log("ageDarma: " + ageDarma);
 console.log("Datum" + date);
+
+let isChrisOlder, isChrisEqual, isDarmaOlder;
+isChrisOlder = (ageChris > ageDarma);
+isChrisEqual = (ageChris == ageDarma);
+isDarmaOlder = (ageDarma > ageChris);
+
+console.log("isChrisOlder: " + isChrisOlder)
+console.log("isChrisEqual: " + isChrisEqual)
+console.log("isDarmaOlder: " + isDarmaOlder) 
