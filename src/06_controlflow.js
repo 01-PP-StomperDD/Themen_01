0461dbc5b6e1290993dd1ca2f68b384a9ced41b7
Stomper90845 <168742587+Stomper90845@users.noreply.github.com>
2024-05-10 10:13:30 +0000

    Tupperware

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -2,19 +2,24 @@
 
 // Deklaration + Assignment
 const ageJohn = 30;
-const ageMark = 30;
+const ageMark = 15;
 
 // Deklaration
-let isJohnOlder, isJohnEqual;
+let isJohnOlder, isJohnEqual, isJohnYounger;
 
 // Test | Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
+isJohnYounger = (ageJohn < ageMark);
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-// console.log("------------------");
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
 
+
+    if   (ageJohn > ageMark) console.log("John ist älter");
+    if   (ageJohn == ageMark) console.log("John und mark sind gleich alt");
+    if   (ageJohn < ageMark) console.log("John ist jünger");
