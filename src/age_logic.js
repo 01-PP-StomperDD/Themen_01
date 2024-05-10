7f633cd8e39711995b7514a6f1a9ab5d1c6703e2
Stomper90845 <168742587+Stomper90845@users.noreply.github.com>
2024-05-10 08:24:00 +0000

    script coy

--- src/age_logic.js
+++ src/age_logic.js
@@ -1,1 +1,12 @@
-let ageChris
+let ageChris, ageDarma;
+const birthYearChris = 1980;
+const birthYearDarma = 1990;
+
+// Berechnung Alter
+let year = 2024;
+ageJohn = year - birthYearChris;
+ageMark = year - birthYearDarma;
+
+// Ausgabe
+console.log("ageJohn: " + ageChris);
+console.log("ageMark: " + ageDarma);
